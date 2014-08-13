81b61cf1339b269b0ef70e7a97b8939930a10516
ebara10 <ebara10@c.kyoai.ac.jp>
2014-08-13 10:55:34 +0900

    Update renshu-5-3.c

--- renshu-5-3.c
+++ renshu-5-3.c
@@ -2,19 +2,25 @@
 
 int main()
 {
-    int sum = 0, p = 0, n = 0;
+    int sum = 0, p = 0, n = 0, max = 0, min = 100;
 
     while(p != -1){
         printf("点数(-1で終了)----");
         scanf("%d", &p);
         if(p != -1) {
+            if(max < p){
+                max = p;
+            }
+            if(min > p){
+                min = p;
+            }
             sum = sum + p;
             n++;
         }
     }
     if(n != 0) {
-        printf("%d人の合計点は%dです。\n", n, sum);
-        printf("平均点は%5.1f点です\n", (double)sum /n);
+        printf("全部の受験者数は%d人です。\n", n);
+        printf("最高点:%d,最低点%d,平均点:%5.1f\n", max, min, (double)sum /n);
     }
     return 0;
 }
