b7ee5425e83001ae323452e2b16d8e816925c366
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-01 20:05:41 +0900

    Update tsuika3-18.c

--- tsuika3-18.c
+++ tsuika3-18.c
@@ -4,20 +4,19 @@ int main()
 {
 
     int month;
-
     printf("何月ですか：");
     scanf("%d", &month);
 
-    if(month == 3 || month == 4 || month == 5){
+    if(month >= 3 && month <= 5){
         printf("春です。\n");
-    } else if(month == 6 || month == 7 || month == 8){
+    } else if(month >= 6 && month <= 8){
         printf("夏です。\n");
-    } else if(month == 9 || month == 10 || month == 11){
+    } else if(month >= 9 &&  month <= 11){
         printf("秋です。\n");
     } else if(month == 12 || month == 1 || month == 2){
         printf("冬です。\n");
-    } else {
-        printf("入力をお間違えではないですか\n");
+    } else if(month > 12 || month < 0) {
+        printf("入力をお間違えではないですか？\n");
     }
 
     return 0;
