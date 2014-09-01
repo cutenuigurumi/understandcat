e71ecc79d17995e08d0e5fbe24028edccd7599ed
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-01 15:24:50 +0900

    Update tsuika-4-5.c

--- tsuika-4-5.c
+++ tsuika-4-5.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 
-int inputNumber1 = 0, inputNumber2 = 0, low = 0, high = 0, check = 0, total = 0;
 
 int main()
 {
-    while(inputNumber1 == inputNumber2){
+int i = 1, inputNumber1 = 0, inputNumber2 = 0, low = 0, high = 0, check = 0, total = 0;
+
+    do {
+        if(i > 1){
+            printf("同じ数値は入れられません\n");
+        }
         printf("二つの整数を入力してください。\n");
         printf("整数1:");
         scanf("%d", &inputNumber1);
 
         printf("数値2:");
         scanf("%d", &inputNumber2);
+        i++;
+    } while(inputNumber1 == inputNumber2);
 
-        if(inputNumber1 == inputNumber2){
-            printf("同じ数値は入れられません\n");
-        }
-    }
     if (inputNumber1 > inputNumber2){
         high = inputNumber1;
         low = inputNumber2;
