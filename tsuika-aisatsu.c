78275479246297c35de1c265062e728ac6e2d708
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-02 20:09:06 +0900

    Update tsuika-aisatsu.c

--- tsuika-aisatsu.c
+++ tsuika-aisatsu.c
@@ -1,25 +1,32 @@
 #include<stdio.h>
+int countCharacters(char *);
 
 int main()
 {
+    int stringLength;
     char name[40];
-    int i, overFourtyFlag = 0;
-
     printf("お名前を40文字以内で:");
     scanf("%[^\n]", name);
 
-    while(name[i] != '\0') {
-        if(i > 40) {
-            printf("40文字以内で入力してください\n");
-            overFourtyFlag = 1;
-            break;
-        }
-        i++;
-    }
-    if(overFourtyFlag == 0){
+    stringLength = countCharacters(name);
+    if(stringLength > 40){
+        printf("40文字以内で入力してください。\n");
+    } else {
         printf("はじめまして、%sさん！\n", name);
     }
-
     return 0;
+}
 
+
+int countCharacters(char *name)
+{
+    int stringLength = 0, countEndFlag = 0;
+    do{
+        if(name[stringLength] == '\0'){
+            countEndFlag = 1;
+            return stringLength;
+        }
+        stringLength++;
+    }while(countEndFlag == 0);
+    return stringLength;
 }
