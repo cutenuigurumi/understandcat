5df938d31ac24bf9c8966c75cf21273b126f5ff9
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-02 10:44:08 +0900

    Create tsuika9-9.c

---
+++ tsuika9-9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int va[5] = {15, 20, 30, 0, 0}, vb[5], i;
+
+    printf("va vb\n");
+    printf("------\n");
+
+    for(i = 0;i < 5; i++){
+        vb[i] = va[i];
+        printf("%2d %2d\n", va[i], va[i]);
+    }
+
+
+}
