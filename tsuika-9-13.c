1fe99e16eee665e5619e6475e68003c5e155bcc8
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-04 09:51:13 +0900

    Update tsuika-9-13.c

--- tsuika-9-13.c
+++ tsuika-9-13.c
@@ -4,23 +4,23 @@
 void put_stringn(const char string[], int times);
 int main()
 {
-    int times, count_Length_Func;
+    int times;
     char string[40];
     printf("文字列を入力してください：");
     scanf("%s", string);
     printf("何回表示しますか：");
     scanf("%d", &times);
 
-        put_stringn(string, times);
+    put_stringn(string, times);
     return 0;
 }
 
 void put_stringn(const char *string, int times)
 {
-    int i = 0;
+    int i = 1;
     do{
         printf("%s", string);
         i++;
-    }while(times > i);
+    }while(i <= times);
+    printf("\n");
 }
-
