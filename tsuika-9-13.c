10202fafc0081d38062b689e4a348cca4df48f51
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-05 15:10:54 +0900

    Update tsuika-9-13.c

--- tsuika-9-13.c
+++ tsuika-9-13.c
@@ -1,38 +1,42 @@
 #include<stdio.h>
 
 void put_stringn(const char string[], int times);
-int count_Length_Func(const char string[]);
+int count_length_func(const char string[]);
 int main()
 {
     int times, count;
     char string[40];
     printf("文字列を入力してください：");
     scanf("%40[^\n]", string);
-//    count = count_Length_Func(string);
+    count = count_length_func(string);
     if(count > 40){
         printf("40文字以上は入力ができません\n");
     } else {
         printf("何回表示しますか：");
         scanf("%d", &times);
+        if(times < 99){
+            put_stringn(string, times);
+        } else {
+            printf("99回以上は入力できませんよ。\nもしくは値が不正っぽいですよ。\n");
+        }
     }
-    put_stringn(string, times);
     return 0;
 }
 
 void put_stringn(const char *string, int times)
 {
     int i = 1;
-    do{
+    while(times >= i){
         printf("%s", string);
         i++;
-    }while(i <= times);
+    }
     printf("\n");
 }
 
-int count_Length_Func(const char *string)
+int count_length_func(const char *string)
 {
 //変数用意
-int endFlag = 0,i = 0,count = 1;
+    int endFlag = 0,i = 0,count = 1;
 
     //\フラッグがたつまで繰り返すはじめ
     do {
