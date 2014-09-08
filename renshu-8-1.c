c6fd9e4244bf076830360d2e1567d97c6fbeb61c
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-08 10:14:57 +0900

    Update renshu-8-1.c

--- renshu-8-1.c
+++ renshu-8-1.c
@@ -2,30 +2,31 @@
 
 int main()
 {
-    int i = 0, studentNumber[40],tmpStudentNumber, countStudent = 1;
+    int i = 0, studentNumber = 0;
+    double tmpStudentRecord,studentRecord[40],average = 0, sum = 0;
+    int endFlag = 0;
 
-    double studentRecord[40],average, sum;
-
-    do{
-        printf("出席番号を入力してください(999)で終了");
-        scanf("%d", &tmpStudentNumber);
-        if(tmpStudentNumber == 999){
+    do {
+        printf("出席番号%d番の100mのタイムを入力してください(終了→ 999)----", i + 1);
+        scanf("%lf", &tmpStudentRecord);
+        if(tmpStudentRecord == 999){
             break;
+        } else if(tmpStudentRecord > 100 || tmpStudentRecord < 0){
+            printf("100秒以上、または値が不正です。\n登録できません。");
         }
-        studentNumber[i] = tmpStudentNumber;
-
-        printf("100mのタイムを入力してください----");
-        scanf("%lf", &studentRecord[i]);
-        sum += studentRecord[i];
-        countStudent++;
+        studentRecord[i] = tmpStudentRecord;
+        sum += tmpStudentRecord;
         i++;
     }while(i < 40);
 
     average = sum / i;
 
-    printf("%d名の100m走の平均タイムは%fです。\n", i, average);
-    
+    printf("%d名の100m走の平均タイムは%5.2fです。\n", i, average);
 
+    while(i > studentNumber){
+        printf("出席番号%d:%5.2f秒です。\n", studentNumber +1, studentRecord[studentNumber]);
+        studentNumber++;
+        }
 
     return 0;
 
