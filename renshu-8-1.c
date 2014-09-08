8998e9c30c23daf5f71c34d40b32727f9e887252
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-08 19:10:05 +0900

    Update renshu-8-1.c

--- renshu-8-1.c
+++ renshu-8-1.c
@@ -2,16 +2,19 @@
 
 int main()
 {
-    int i = 0, studentNumber = 0;
+    int i = 0, studentNumber = 0, errorFlag = 0;
     double tmpStudentRecord,studentRecord[40],average = 0, sum = 0;
 
     do {
         printf("出席番号%d番の100mのタイムを入力してください(終了→ 999)----", i + 1);
         scanf("%lf", &tmpStudentRecord);
+        //tmpStudentRecordが999だったら処理を抜ける
         if(tmpStudentRecord == 999){
             break;
+        //エラー処理
         } else if(tmpStudentRecord > 100 || tmpStudentRecord <= 0){
             printf("100秒以上、または値が不正です。\n登録できません。\n");
+            break;
         } else {
             studentRecord[i] = tmpStudentRecord;
             sum += tmpStudentRecord;
@@ -21,13 +24,13 @@ int main()
 
     average = sum / i;
 
-    printf("%d名の100m走の平均タイムは%5.2fです。\n", i, average);
-
+    if(errorFlag == 0){
+        printf("%d名の100m走の平均タイムは%5.2fです。\n", i, average);
+    }
     while(studentNumber < i){
         printf("出席番号%d:%5.2f秒です。\n", studentNumber +1, studentRecord[studentNumber]);
         studentNumber++;
     }
-
     return 0;
 
 
