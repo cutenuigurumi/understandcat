83e5e9c7d9d0914f7b6e114c33692197b86cc26a
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-09 11:55:45 +0900

    Create renshu-8-3.c

---
+++ renshu-8-3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int countCharacter(char *);
+int main()
+{
+    int countLength = 0;
+    char *inputCharacter;
+    //文字入力
+    printf("文字を入力してください。");
+    scanf("%s", inputCharacter);
+
+    //文字のカウント処理
+    countLength = countCharacter(inputCharacter);
+
+    while(countLength >= 0){
+        printf("%c", inputCharacter[countLength]);
+        countLength--;
+    }
+    printf("\n");
+}
+
+int countCharacter(char *inputCharacter)
+{
+    int countLength = 0;
+    while(inputCharacter[countLength] != '\0'){
+        countLength++;
+//      printf("%d", countLength);
+    }
+        return (countLength);
+
+}
+
