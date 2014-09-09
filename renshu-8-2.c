bb2581adda56c5d064130e67d9d23834b2f7c9bc
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-09 10:33:25 +0900

    Update renshu-8-2.c

--- renshu-8-2.c
+++ renshu-8-2.c
@@ -2,17 +2,24 @@
 
 int main()
 {
-    int day = 0, inputDate = 0;
+    int day = 0, inputDate = 0, endFlag = 0;
     char *charDay[] = {"土","日","月","火","水","木","金"};
 
-    printf("日付を入力して下さい。----");
-    scanf("%d", &inputDate);
+    do{
+        printf("日付を入力して下さい。----");
+        if(scanf("%d", &inputDate) != 1){
+            scanf("%*s");
+            printf("値が不正です。\n");
+            continue;
+        }
+        //31より大きかったり、マイナスだったらエラー
+        if(inputDate > 31 || inputDate <= 0){
+            printf("正しい日付を入力してください。\n");
+            continue;
+        }
+            day = inputDate % 7;
+            printf("%s曜日です\n", charDay[day]);
+            endFlag = 1;
+    } while(endFlag == 0);
 
-    //31より大きかったり、マイナスだったらエラー
-    if(inputDate > 31 || inputDate <= 0){
-        printf("正しい日付を入力してください。\n");
-    } else {
-        day = inputDate % 7;
-        printf("%s曜日です\n", charDay[day]);
-    }
 }
