1baf9fb1afa60f6e61c7b632ab4aaeb5b0a7b956
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-12 21:52:27 +0900

    Update renshu-8-3.c

--- renshu-8-3.c
+++ renshu-8-3.c
@@ -1,38 +1,48 @@
-#include<stdio.h>
+#include<string.h>
 
-char turnOver(const char *);
+
+char* turnOver(char *);
 int main()
 {
     //reversedString[0]には'\0'が入っているので配列だけど1からスタート。
-    int k = 1;
     char inputString[40], reversedString[40];
     //文字入力
     printf("文字を入力してください。");
     scanf("%s", inputString);
-    reversedString = turnOver(inputString);
 
-    printf("%s", reversedString);
+    printf("%s", turnOver(inputString));
     return 0;
 }
 //inputStringの配列の先頭アドレスを受け取る
-char turnOver(const char inputString[])
+char* turnOver(char* inputString)
 {
-    char reversedString[40];
+    char tmpString;
     int i = 0,j = 0;
     //何文字かを確認
 
     while(inputString[i] != '\0'){
         i++;
     }
+    //最後にぬるが入ってるので、マイナス１
 
+    printf("%d", i);
     //逆向きにして、別の配列に保存
-    while(i >= 0){
-        reversedString[j] = inputString[i];
-        printf("%c", reversedString[j]);
+    while(i > j){
+        tmpString = inputString[i];
+        inputString[i] = inputString[j];
+        inputString[j] = tmpString;
         j++;
         i--;
+//      printf("くるくるー\n");
     }
+    int k = 0;
+    while(k < j){
+    printf("%c", inputString[k]);
+    k++;
+    }
+    inputString[j] = '\0';
+    printf("%s", inputString);
 
     printf("\n");
-    return *reversedString;
+    return inputString;
 }
