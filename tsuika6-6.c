53eacdc51d6cd6ba7034d4068b4341cc8b2d4712
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-18 11:13:37 +0900

    Update tsuika6-6.c

--- tsuika6-6.c
+++ tsuika6-6.c
@@ -7,13 +7,13 @@ int main(){
 
     printf("警告音を何回ならしますか？");
     scanf("%d", &no);
-    if(no > 10){
-        printf("10回以上はならせません。");
-    } else if(no < 0) {
-        printf("入力値がエラーみたいです。");
+    if(no < 0 || no > 1000) {
+        printf("入力値がエラーみたいです。\n");
+    } else if(no > 10){
+        printf("10回以上はならせません。\n");
     } else {
         alert(no);
-        printf("%d回ならしました。", no);
+        printf("%d回ならしました。\n", no);
     }
     return 0;
 
@@ -22,7 +22,7 @@ int main(){
 void alert(int no)
 {
     int i;
-    for(i = 0; i < no; i++){
+    for(i = 1; i <= no; i++){
         printf("\a");
     }
 
