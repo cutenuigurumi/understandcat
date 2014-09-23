4986b6261400e909a5f5a3a22e962420660af321
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-23 17:38:44 +0900

    エラーチェック失敗
    エラーを関数化しようとして失敗

--- renshu-9-1.c
+++ renshu-9-1.c
@@ -1,5 +1,6 @@
 #define STUDENTNO 3
 #include<stdio.h>
+#include<string.h>
 
 struct Record {
 
@@ -7,32 +8,63 @@ struct Record {
     int japanese;
     int science;
 };
+void lookfor(struct Record *, char *);
 
 int main()
 {
-
-    struct Record student[STUDENTNO];
-    int i, j;
+    int i = 0;
     char lookForThisName[32];
+    struct Record student[STUDENTNO];
 
-    for(i = 0; i < STUDENTNO; i++) {
+    while(i < STUDENTNO) {
         printf("生徒の名前----");
-        scanf("%s", student[i].name);
+        if(scanf("%s", student[i].name) != 1){
+            scanf("%*s");
+            continue;
+        }
         printf("国語の成績----");
-        scanf("%d", &student[i].japanese);
+        if(scanf("%d", &student[i].japanese) != 1){
+            scanf("%*s");
+            continue;
+        }
+        is_check(student[i].japanese);
         printf("理科の成績----");
-        scanf("%d", &student[i].science);
+        if(scanf("%d", &student[i].science) != 1){
+            scanf("%*s");
+            continue;
+        }
+        is_check(student[i].science);
+    i++;
     }
-
     printf("探したい人の名前を入れてください。");
     scanf("%s", lookForThisName);
 
-    printf("DMSG lookForThisName → %s\n", lookForThisName);
-    for(j = 0; j < STUDENTNO; j++){
-        if(lookForThisName == student[j].name){
-            printf("%s, %d, %d", student[j].name, student[j].japanese, student[j].science);
-        }
-        printf("DMSG student[j].name → %s\n", student[j].name);
-    }
+    lookfor(student, lookForThisName);
 
+}
+
+void lookfor(struct Record *student, char *lookForThisName)
+{
+    int i, j, k, studentNameLength;
+
+    //i 生徒の人数
+    for(i = 0; i < STUDENTNO; i++){
+        //j 文字を指定
+        studentNameLength = strlen(student[i].name);
+        //k studentNameLengthの文字数
+        for(k = 0; k < studentNameLength; k++){
+            if(lookForThisName[k] == student[i].name[k]){
+                printf("氏名:%s, 国語:%d点, 英語:%d点\n", student[i].name, student[i].japanese, student[i].science);
+                break;
+            }
+        }
+    }
+}
+
+void is_check(*score)
+{
+
+    if(score > 100 | score < 0){
+        printf("入力値がエラーみたいです。。\n");
+    }
 }
