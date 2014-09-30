e4b7d0c3263c39bb9e0cb8bd98f0c662a218c4f0
ebara10 <ebara10@c.kyoai.ac.jp>
2014-09-30 21:54:41 +0900

    Create renshu-10-1.c

---
+++ renshu-10-1.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<string.h>
+
+int write_addresslist(FILE *);
+void lookfor_addresslist(FILE *);
+int main()
+{
+    FILE *fp;
+    char filename[256];
+    int endWrite = 0,decision = 0, errorFlag = 0;
+
+    fp = fopen("renshu-test-10-1.txt", "a+");
+
+    if(fp == NULL){
+        perror("ファイルのオープンに失敗しました。\n");
+        return -1;
+    }
+
+    do{
+        printf("登録…1 / 読み込み…0\n");
+        if(scanf("%d", &decision) != 1){
+            scanf("%*s");
+        }
+        if(decision == 1){
+            //書き込みモード
+            while(endWrite == 0){
+                endWrite = write_addresslist(fp);
+            }
+        } else if(decision == 0){
+            //読み込みモード
+            lookfor_addresslist(fp);
+        } else {
+            printf("エラーです！");
+            errorFlag = 1;
+        }
+    }while(errorFlag == 1);
+
+
+    fclose(fp);
+    return 0;
+}
+
+int write_addresslist(FILE *fp)
+{
+    char name[16], address[100], tel[20];
+    int decision = 0;
+    char format[] = "%s %-4.1f %-4.1f\n";
+
+    printf("氏名:");
+    scanf("%s", name);
+
+    printf("住所:");
+    if(scanf("%s", address) != 1){
+        scanf("%*s");
+    }
+    printf("電話番号:");
+    if(scanf("%s", tel) != 1){
+        scanf("%*s");
+    }
+    fflush( stdin );
+
+    fprintf(fp, "%s, %s, %s\n", name, address, tel);
+    printf("書き込みました。\n");
+
+    //decisionに1か0以外が入れられたらループ
+    //1なら終了、0なら続ける
+    do{
+        printf("終了しますか(Yes…1/ No…0):");
+        if(scanf("%d", &decision) != 1){
+            printf("数値以外は入力できません");
+            scanf("%*s");
+            continue;
+        }
+        if(decision == 1){
+            printf("終了します\n");
+            return decision;
+        }else if(decision == 0){
+            break;
+        }else{
+            printf("1と0以外は入れられません。入力ミスです\n");
+            continue;
+        }
+    }while(decision == 0);
+    return decision;
+}
+
+void lookfor_addresslist(FILE *fp)
+{
+
+    char c;
+    while(fscanf(fp, "%c", &c) != EOF){
+        printf("%c", c);
+    }
+}
