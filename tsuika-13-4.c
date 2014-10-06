cceb7c5bec8f79dafc5091097dcc281ad8e88204
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-06 19:42:45 +0900

    Update tsuika-13-4.c

--- tsuika-13-4.c
+++ tsuika-13-4.c
@@ -1,50 +1,68 @@
-#include<stdio.h>
+nclude<stdio.h>
 #include<string.h>
+#define FILENAME "renshu-test-10-1.txt"
 
-int write_profile(FILE *);
-
+int writeAddressList();
+void lookforAddressList();
 int main()
 {
-    FILE *fp;
-    char filename[256];
-    int endWrite = 0;
+    int endWrite = 0,decision = 0, errorFlag = 0;
 
-    fp = fopen("renshu-13-4.txt", "a");
+    do{
+        printf("登録…1 / 読み込み…0\n");
+        if(scanf("%d", &decision) != 1){
+            scanf("%*s");
+        }
+        if(decision == 1){
+            //書き込みモード
+            while(endWrite == 0){
+                endWrite = writeAddressList();
+            }
+        } else if(decision == 0){
+            //読み込みモード
+            lookforAddressList();
+        } else {
+            printf("エラーです！");
+            errorFlag = 1;
+        }
+    }while(errorFlag == 1);
+
+
+    return 0;
+}
+/* ----------------------------------------------------------- *
+ * writeAddressList : 氏名、住所、電話番号をrenshu-test-10-1.txtに
+ * 書き込んでいく。
+ *   引数：なし
+ *   戻り値：decision 書き込みを続ける(1)か、終了する(0)かの判断。
+ *   エラー時の処理：バッファをクリア
+ * ----------------------------------------------------------- */
+int writeAddressList()
+{
+
+    char name[20], address[20], tel[20];
+    int decision = 0;
+    FILE *fp;
+    fp = fopen("renshu-test-10-1.txt", "a");
     if(fp == NULL){
         perror("ファイルのオープンに失敗しました。\n");
         return -1;
     }
-    //終了なら1,続けるなら0が帰ってくる
-    while(endWrite == 0){
-        endWrite = write_profile(fp);
-    }
-    fclose(fp);
-    return 0;
-}
 
-int write_profile(FILE *f)
-{
-    char name[16];
-    int decision = 0;
-    double weight = 0,height = 0;
-    char *format = "%s %-4.1f %-4.1f\n";
+    printf("氏名:");
+    scanf("%s", name);
 
-    fflush( stdin );
-    printf("名前:");
-    fgets(name, sizeof(name), stdin);
-    name[strlen(name) -1] = '\0';
-
-    printf("身長:");
-    if(scanf("%lf", &height) != 1){
+    printf("住所:");
+    if(scanf("%s", address) != 1){
         scanf("%*s");
     }
-    printf("体重:");
-    if(scanf("%lf", &weight) != 1){
+    printf("電話番号:");
+    if(scanf("%s", tel) != 1){
         scanf("%*s");
     }
     fflush( stdin );
 
-    fprintf(f, format, name, height, weight);
+    fprintf(fp, "%s,%s,%s\n", name, address, tel);
     printf("書き込みました。\n");
 
     //decisionに1か0以外が入れられたらループ
@@ -60,12 +78,57 @@ int write_profile(FILE *f)
             printf("終了します\n");
             return decision;
         }else if(decision == 0){
-            return decision;
+            break;
         }else{
             printf("1と0以外は入れられません。入力ミスです\n");
+            continue;
         }
     }while(decision == 0);
+    return decision;
+}
+/* ----------------------------------------------------------- *
+ * lookforAddressList : 検索したい名前を入力すると、
+ * 一致した名前、住所、電話番号を画面に出力する
+ *   引数：なし
+ *   戻り値：なし
+ *   エラー時の処理：バッファをクリア
+ * ----------------------------------------------------------- */
+void lookforAddressList()
+{
+    FILE *fp;
+    fp = fopen("renshu-test-10-1.txt", "r");
+    char buffer[100],lookforName[20], *name, *address, *tel, yesno[6];
+    int no = 0;
 
-    return 0;
+    if(fp == NULL){
+        perror("ファイルのオープンに失敗しました。\n");
+    }
+    printf("検索したい名前を入れて下さい----");
+    if(scanf("%s", lookforName) != 1){
+        scanf("%*s");
+    }
+
+    while(fgets(buffer, sizeof(buffer), fp) !=NULL){
+        name = strtok(buffer, ",");
+        address = strtok(NULL, ",");
+        tel = strtok(NULL, ",");
+        if(strstr(name, lookforName) != NULL) {
+        printf("名前:%-s\n住所:%-s\nTEL:%-s", name,address,tel);
+        }
+        printf("さらに検索を続けますか？(y/n)----");
+        if(scanf("%s", yesno) != 1){
+            scanf("%*s");
+        }
+        if(yesno[0] == 'y' || yesno[0] == 'Y'){
+            continue;
+        } else if(yesno[0] =='n' || yesno[0] == 'N') {
+            printf("終了します\n");
+            break;
+        } else {
+            printf("エラーです。終了します。\n");
+            break;
+        }
+    }
+
 
 }
