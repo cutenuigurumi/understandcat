5fe4dab42e10c5189d99ba51b944efb8cbf29967
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-07 16:52:00 +0900

    Update tsuika4-19.c

--- tsuika4-19.c
+++ tsuika4-19.c
@@ -3,8 +3,10 @@ int main()
 {
     int i = 1;
     while(i <= 50){
+        //5で割り切れるなら改行
         if(i % 5 == 0){
             printf("%2d\n", i);
+        //割り切れなければ後ろに数字が続くのでスペース
         } else {
             printf("%2d ", i);
         }
