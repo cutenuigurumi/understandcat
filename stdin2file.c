e32fe12429873d83b0629ddd517eb10b8d47a28e
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-10 18:39:35 +0900

    Update stdin2file.c

--- stdin2file.c
+++ stdin2file.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define FILENAME "filetest1.txt"
-
+#define LENGTH 10
 
 int main(int argc, char *argv[])
 {
@@ -9,15 +9,19 @@ int main(int argc, char *argv[])
     fp = fopen(FILENAME, "a");
     if(fp == NULL){
         perror("ファイルのオープンに失敗しました。\n");
-        return -1;
+        exit (-1);
     }
 
-    char string[10];
+    char string[LENGTH];
     fputs("文字列を入力してください:" , stdout);
     fgets(string, sizeof(string), stdin);
-    fprintf(fp, "%s\n", string);
-    fputs("文字列を書き込みました。");
+    if(string[LENGTH - 1] == '\0'){
+        fprintf(stdout, "文字が長過ぎます。10文字を越えているものは表示されません\n");
+    }
+    fputs(string, fp);
+    fputs("文字列を書き込みました。\n", stdout);
 
     fclose(fp);
     exit(0);
 }
+~     
