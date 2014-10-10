280e5b14a39f5cae2cc36d839c7eec2643a49275
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-10 19:34:59 +0900

    Update renshu-10-1.c

--- renshu-10-1.c
+++ renshu-10-1.c
@@ -4,6 +4,7 @@
 
 int writeAddressList();
 void lookforAddressList();
+int isErrorString(char *);
 int main()
 {
     int endWrite = 0,decision = 0, errorFlag = 0;
@@ -31,11 +32,10 @@ int main()
     return 0;
 }
 /* ----------------------------------------------------------- *
- * writeAddressList : 氏名、住所、電話番号をrenshu-test-10-1.txtに
+ * writeAddressList : 氏名、住所、電話番号をファイル名に
  * 書き込んでいく。
  *   引数：なし
  *   戻り値：decision 書き込みを続ける(1)か、終了する(0)かの判断。
- *   エラー時の処理：バッファをクリア
  * ----------------------------------------------------------- */
 int writeAddressList()
 {
@@ -43,24 +43,23 @@ int writeAddressList()
     char name[20], address[20], tel[20];
     int decision = 0;
     FILE *fp;
-    fp = fopen("renshu-test-10-1.txt", "a");
+    fp = fopen(FILENAME, "a");
     if(fp == NULL){
         perror("ファイルのオープンに失敗しました。\n");
         return -1;
     }
 
     printf("氏名:");
-    scanf("%s", name);
+    decision = isErrorString(name);
+    if(decision == -1){
+        printf("エラーです。\n");
+    }
 
     printf("住所:");
-    if(scanf("%s", address) != 1){
-        scanf("%*s");
-    }
+    decision = isErrorString(address);
+
     printf("電話番号:");
-    if(scanf("%s", tel) != 1){
-        scanf("%*s");
-    }
-    fflush( stdin );
+    decision = isErrorString(tel);
 
     fprintf(fp, "%s,%s,%s\n", name, address, tel);
     printf("書き込みました。\n");
@@ -77,7 +76,8 @@ int writeAddressList()
         if(decision == 1){
             printf("終了します\n");
             return decision;
-        }else if(decision == 0){
+        }
+        if(decision == 0){
             break;
         }else{
             printf("1と0以外は入れられません。入力ミスです\n");
@@ -91,20 +91,19 @@ int writeAddressList()
  * 一致した名前、住所、電話番号を画面に出力する
  *   引数：なし
  *   戻り値：なし
- *   エラー時の処理：バッファをクリア
  * ----------------------------------------------------------- */
 void lookforAddressList()
 {
     FILE *fp;
-    fp = fopen("renshu-test-10-1.txt", "r");
-    char buffer[100],lookforName[20], *name, *address, *tel, yesno[6];
+    fp = fopen(FILENAME, "r");
+    char buffer[100],searchName[20], *name, *address, *tel, yesno[6];
     int no = 0;
 
     if(fp == NULL){
         perror("ファイルのオープンに失敗しました。\n");
     }
     printf("検索したい名前を入れて下さい----");
-    if(scanf("%s", lookforName) != 1){
+    if(scanf("%s", searchName) != 1){
         scanf("%*s");
     }
 
@@ -112,7 +111,7 @@ void lookforAddressList()
         name = strtok(buffer, ",");
         address = strtok(NULL, ",");
         tel = strtok(NULL, ",");
-        if(strstr(name, lookforName) != NULL) {
+        if(strstr(name, searchName) != NULL) {
         printf("名前:%-s\n住所:%-s\nTEL:%-s", name,address,tel);
         }
         printf("さらに検索を続けますか？(y/n)----");
@@ -121,7 +120,8 @@ void lookforAddressList()
         }
         if(yesno[0] == 'y' || yesno[0] == 'Y'){
             continue;
-        } else if(yesno[0] =='n' || yesno[0] == 'N') {
+        }
+        if(yesno[0] =='n' || yesno[0] == 'N') {
             printf("終了します\n");
             break;
         } else {
@@ -129,6 +129,19 @@ void lookforAddressList()
             break;
         }
     }
+}
+/* ----------------------------------------------------------- *
+ * isErrorString : 引数がエラーかどうかを判断して、エラーがあれば-1
+ * を返して、バッファをクリア
+ *   引数: char型の変数
+ *   戻り値：何も無ければ0, エラーがあれば-1
+ * ----------------------------------------------------------- */
 
-
+int isErrorString(char *string)
+{
+    if(scanf("%s", string) != 1){
+        scanf("%*s");
+        return -1;
+    }
+    return 0;
 }
