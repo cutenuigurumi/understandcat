637620c1efc4fd8c5bfbdfa7e556c297534fd602
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-14 20:14:57 +0900

    Update tsuika4-27.c

--- tsuika4-27.c
+++ tsuika4-27.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<string.h>
+#define STRINGLENGTH 120
 
 void printPyramid(int);
 int main()
@@ -11,9 +13,14 @@ int main()
             scanf("%*s");
             printf("数値が不正です。戻ります。\n");
             errorFlag = 1;
-        } else {
-            errorFlag = 0;
+            continue;
         }
+        if(inputStep > 50){
+            printf("50段以上は入力できません。\n");
+            errorFlag = 1;
+            continue;
+        }
+        errorFlag = 0;
     }while(errorFlag == 1);
     printPyramid(inputStep);
     return 0;
@@ -27,20 +34,23 @@ int main()
 void printPyramid(int step)
 {
     int i = 0, j = 0, k = 0, no = 1, space = 0;
-    /*space スペースを埋める数 k あと何個スペースで埋めるか
+    char string[STRINGLENGTH];
+    /*space スペースを埋める数, k あと何個スペースで埋めるか
     *stepは30行目のwhile文で使っているので変えない。 */
-    k = space = step;
+    space = step;
     while(i < step){
+        //stringは1行単位なので、ここで初期化しないといけない。
+        memset(string, '\0', STRINGLENGTH);
         for(k = space;0 < k; k--){
-            printf(" ");
+            strcat(string, " ");
         }
         //noが*の数
         for(j = 0; no > j; j++){
-            printf("*");
+            strcat(string, "*");
         }
         no = no + 2;
         i++;
         space--;
-        printf("\n");
+        printf("%s\n", string);
     }
 }
