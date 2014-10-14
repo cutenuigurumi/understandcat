39cc902f36ab7c8d3820827a0e99eb4075fb1a49
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-14 19:33:00 +0900

    Update tsuika4-27.c

--- tsuika4-27.c
+++ tsuika4-27.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
+void printPyramid(int);
 int main()
 {
-    int inputStep = 0,no = 1, i = 0, j = 0, errorFlag = 0;
+    int inputStep = 0, errorFlag = 0;
     printf("ピラミッドを造りましょう。\n");
     do{
         printf("何段ですか？:");
@@ -15,13 +16,34 @@ int main()
         }
     }while(errorFlag == 1);
 
-    //noが*の個数
-    while(inputStep > i){
+    printPyramid(inputStep);
+
+    return 0;
+}
+
+
+/* ----------------------------------------------------------- *
+ * printPyramid : ピラミッドを*で描写します。
+ *   引数：Step：ピラミッドの段数
+ *   戻り値：無し
+ * ----------------------------------------------------------- */
+void printPyramid(int step)
+{
+    int i = 0, j = 0, k = 0, no = 1, space = 0;
+    /*space スペースを埋める数 k あと何個スペースで埋めるか
+    *stepは30行目のwhile文で使っているので変えない。 */
+    k = space = step;
+    while(i < step){
+        for(k = space;0 < k; k--){
+            printf(" ");
+        }
+        //noが*の数
         for(j = 0; no > j; j++){
             printf("*");
         }
         no = no + 2;
         i++;
+        space--;
         printf("\n");
     }
 }
