21f42d726318df75201b85a09d9b33aab7259e0c
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-16 10:27:49 +0900

    Update stdin2file.c

--- stdin2file.c
+++ stdin2file.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define FILENAME "filetest1.txt"
-#define LENGTH 10
+#define STRINGLENGTH 10
+#define MESSAGELENGTH 30
 
 int main(int argc, char *argv[])
 {
+    char message[MESSAGELENGTH];
+    char string[STRINGLENGTH];
+
     FILE *fp;
     fp = fopen(FILENAME, "a");
     if(fp == NULL){
@@ -12,16 +17,26 @@ int main(int argc, char *argv[])
         exit (-1);
     }
 
-    char string[LENGTH];
-    fputs("文字列を入力してください:" , stdout);
-    fgets(string, sizeof(string), stdin);
-    if(string[LENGTH - 1] == '\0'){
-        fprintf(stdout, "文字が長過ぎます。10文字を越えているものは表示されません\n");
-    }
-    fputs(string, fp);
-    fputs("文字列を書き込みました。\n", stdout);
+    do{
+        fputs("文字列を入力してください:" , stdout);
+        fgets(string, sizeof(string), stdin);
+        if(string[0] == '\n'){
+            printf("何も入力されていません。\n");
+            continue;
+        }
+        if (strchr(string, '\n') != NULL) {
+            string[STRINGLENGTH - 1] = '\0';
+        } else {
+            printf("文字数が多すぎます。%s\n", string);
+            // 入力ストリームをクリアする処理。
+            while(getchar() != '\n');
+            continue;
+        }
+        fputs(string, fp);
+        fputs("文字をファイルに書き込みました\n", stdout);
+        break;
+    }while(1);
 
     fclose(fp);
     exit(0);
 }
-~     
