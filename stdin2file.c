f6aa146a78eeee146d009d2160b6c5f228734fa8
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-17 15:29:19 +0900

    Update stdin2file.c

--- stdin2file.c
+++ stdin2file.c
@@ -3,18 +3,33 @@
 #include<string.h>
 #define LENGTH 10
 #define MESSAGELENGTH 30
+#define FEXTENSION1 "txt"
+#define FEXTENSION2 "html"
 
+int checkExtension(const char *);
 int main(int argc, char *argv[])
 {
-    char message[LENGTH], string[LENGTH], filename[LENGTH];
+    char extension[LENGTH], message[LENGTH], string[LENGTH], filename[LENGTH];
+    int extensionFlag = 0;
 
     FILE *fp;
-    strcpy(filename, argv[1]);
-    if(filename[0] == '\0'){
-        fprintf(stderr, "ファイル名が入力されていません。。\n");
+    //引数の数チェック
+    if(argc > 2){
+        fprintf(stderr, "引数が多すぎます。\n");
         exit(-1);
     }
-    strcat(filename, ".txt");
+    if(argc == 1){
+        fprintf(stderr, "引数がありません\n");
+        exit(-1);
+    }
+    strcpy(filename, argv[1]);
+
+    //.以降を見る処理に
+    if(checkExtension(filename) != 0){
+        fprintf(stderr, "拡張子が違います\n");
+        exit(-1);
+    }
+
 
     fp = fopen(filename, "a");
     if(fp == NULL){
@@ -44,4 +59,33 @@ int main(int argc, char *argv[])
 
     fclose(fp);
     exit(0);
+}
+
+/* ----------------------------------------------------------- *
+ * checkExtension: 拡張子がtxtかhtmlかをチェックする。
+ *   引数：filename
+ *   戻り値：0拡張子がtxtかhtmlだった。1それ以外。
+ * ----------------------------------------------------------- */
+int checkExtension(const char *filename)
+{
+    char extension1[] = FEXTENSION1, extension2[] = FEXTENSION2, *bufferExtension,  buffer[LENGTH];
+
+    strcpy(buffer, filename);
+
+    //拡張子以下を切り取る
+    bufferExtension = strtok(buffer, ".");
+    bufferExtension = strtok(NULL, ".");
+
+    printf("DMSG %s\n", bufferExtension);
+
+    //拡張子同士を比較同じなら0を返す
+
+    if(strcmp(bufferExtension, extension1) == 0){
+        return 0;
+    }
+    if(strcmp(bufferExtension, extension2) == 0){
+        return 0;
+    } else {
+        return 1;
+    }
 }
