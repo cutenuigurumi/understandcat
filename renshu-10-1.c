7a71e76a0b7c9961c24dba68b06ef08498287008
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-26 14:10:14 +0900

    Update renshu-10-1.c

--- renshu-10-1.c
+++ renshu-10-1.c
@@ -3,29 +3,33 @@
 #define FILENAME "renshu-test-10-1.txt"
 #define LENGTH 10
 
-int writeAddressList();
-void lookforAddressList();
-void clearBuffer();
+//ファイルポインタを渡す
+int writeAddressList(FILE *);
+void lookforAddressList(FILE *);
+//scanfIntの中で、数値を書き換えるので、intのポインタを参照渡しする
+int scanfInt(int *);
 int main()
 {
-    int endWrite = 0,decision = 0, errorFlag = 0;
+    int endWrite = 0,decision = 1, errorFlag = 0;
+    FILE *fp;
+    char filename[20] = FILENAME;
 
     do{
         printf("登録…1 / 読み込み…0\n");
-        if(scanf("%d", &decision) != 1){
-            clearBuffer();
-            errorFlag = 1;
+        errorFlag = scanfInt(&decision);
+        if(errorFlag != 0){
+            fprintf(stderr, "数値以外のものが入力されました。戻ります。\n");
             continue;
         }
         if(decision == 1){
             //書き込みモード
             while(endWrite == 0){
-                endWrite = writeAddressList();
+                endWrite = writeAddressList(fp);
             }
             break;
         } else if(decision == 0){
             //読み込みモード
-            lookforAddressList();
+            lookforAddressList(fp);
             break;
         } else {
             printf("エラーです！");
@@ -42,15 +46,14 @@ int main()
  *   引数：なし
  *   戻り値：decisionFlag 書き込みを続ける(1)か、終了する(0)かの判断。
  * ----------------------------------------------------------- */
-int writeAddressList()
+int writeAddressList(FILE *fp)
 {
 
     char name[LENGTH], address[LENGTH], tel[LENGTH];
     int decisionFlag = 0;
-    FILE *fp;
     fp = fopen(FILENAME, "a");
     if(fp == NULL){
-        perror("ファイルのオープンに失敗しました。\n");
+        fprintf(stderr, "ファイルのオープンに失敗しました。\n");
         return -1;
     }
 
@@ -70,9 +73,8 @@ int writeAddressList()
     *1なら終了、0なら続ける*/
     do{
         printf("終了しますか(Yes…1/ No…0):");
-        if(scanf("%d", &decisionFlag) != 1){
-            printf("数値以外は入力できません");
-            clearBuffer();
+        if(scanfInt(&decisionFlag) != 0){
+            fprintf(stderr, "数値以外は入力できません");
             continue;
         }
         if(decisionFlag == 1){
@@ -94,9 +96,8 @@ int writeAddressList()
  *   引数：なし
  *   戻り値：なし
  * ----------------------------------------------------------- */
-void lookforAddressList()
+void lookforAddressList(FILE *fp)
 {
-    FILE *fp;
     fp = fopen(FILENAME, "r");
     char buffer[100],searchName[20], *name, *address, *tel, yesno[6];
     int no = 0;
@@ -106,7 +107,6 @@ void lookforAddressList()
     }
     printf("検索したい名前を入れて下さい----");
     if(scanf("%s", searchName) != 1){
-        clearBuffer();
     }
 
     while(fgets(buffer, sizeof(buffer), fp) !=NULL){
@@ -132,13 +132,18 @@ void lookforAddressList()
     }
 }
 /* ----------------------------------------------------------- *
- * clearBuffer : バッファをクリア、エラーメッセージをプリント
- *   引数: なし
- *   戻り値:なし
+ * scanfInt : ユーザが数値を入力、数値以外が入力されたら
+ * バッファを削除して、errorFlagを返す
+ *   引数: 数値型の変数
+ *   戻り値:errorFlag 0→正常に入力。　-1→エラー
  * ----------------------------------------------------------- */
 
-void clearBuffer()
+int scanfInt(int *number)
 {
-    scanf("%*s");
-    printf("数値以外のものが入力されました。\n");
+    int errorFlag = 0;
+    if(scanf("%d", number) != 1){
+        scanf("%*s");
+        errorFlag = 1;
+    }
+    return errorFlag;
 }
