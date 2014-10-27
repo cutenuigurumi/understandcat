bc96a3692dd5a1268fb53375d87de867096c62cf
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-27 21:51:39 +0900

    Update get.c

--- get.c
+++ get.c
@@ -5,16 +5,18 @@
 #define METHOD1 "GET"
 #define METHOD2 "POST"
 #define SIZES 100
-#define SIZEFILE 20
-#define SIZEMETHOD 5
-#define SIZEPATH 30
+#define SIZEFILE 20 
+#define METHOD 5
+#define PATH 50
+#define COOKIE 20
 
 int main()
 {
     FILE *fp;
-    char s[SIZES], filename[SIZEFILE], *method, *path, *mod;
+    //*methodを使用するのは、strtok関数が、区切った文字列へのポインタを返す変数のため。method[METHOD]は動かない。
+    char s[SIZES], filename[SIZEFILE], *bufferMethod, method[5], *bufferPath, path[PATH], *bufferCookie, cookie[COOKIE];
 
-    int i = 0, count = 1, k = 0;
+    int i = 0, count = 1,j = 0;
 
     fp = fopen(FILENAME, "r");
     if(fp == NULL){
@@ -25,23 +27,27 @@ int main()
     while(fgets(s, sizeof(s), fp) != NULL){
         //1行目の時だけの処理：GETかPOST と パスの取得
         if(count == 1){
-            method = strtok(s, " ");
-            path = strtok(NULL, " ");
-            mod = strtok(NULL, " ");
-            fputs(path, stdout);
+            bufferMethod = strtok(s, " ");
+            strcpy(method, bufferMethod);
+            bufferPath = strtok(NULL, " ");
+            strcpy(path, bufferPath);
             if(strcmp(method, METHOD1) == 0){
                 fputs("このメソッドはGETです\n", stdout);
             }
             if(strcmp(method, METHOD2) == 0){
-                fputs("このメソッドはPOSTです\n", stdout);
+                fputs("このメソッドはPOSTです\n", stdout); 
             }
             //ここでiをプラスしないと次でスペースから始まってしまう。
-        }
+        } 
+        if(count == 8){
+            bufferCookie = strtok(s, ":");  
+            bufferCookie = strtok(NULL, "\n");
+            strcpy(cookie, bufferCookie);
+        }   
         count++;
-    }
-    fputs(path ,stdout);
-    fputs("\n" ,stdout);
-    fputs(s, stdout);
+    }       
+    fprintf(stdout, "パスは%s\n", path);
+    fprintf(stdout, "クッキーの値は%s\n", cookie);
     fclose(fp);
     exit(0);
-}
+}      
