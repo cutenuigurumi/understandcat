5e7c2fdd7ff574325712be035e206f72f11a7f14
ebara10 <ebara10@c.kyoai.ac.jp>
2014-10-28 20:24:49 +0900

    Update get.c

--- get.c
+++ get.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define FILENAME "get1.txt"
-#define METHOD1 "GET"
-#define METHOD2 "POST"
-#define SIZES 100
+#define FILENAME "POST.txt"
+#define SIZES 200
 #define SIZEFILE 20
 #define METHOD 5
 #define PATH 50
@@ -16,7 +14,7 @@ int main()
     //配列を使用せず、ポインタを使用するのはstrtok関数が、区切った文字列へのポインタを返す変数のため。
     char s[SIZES], filename[SIZEFILE], *bufferMethod, method[METHOD], *bufferPath, path[PATH], *bufferCookie, cookie[COOKIE];
 
-    int i = 0, count = 1,j = 0, ret = 0;
+    int i = 0, firstTime = 0,j = 0, ret = 0, postFlag = 0,paragrahFlag = 0;
 
     fp = fopen(FILENAME, "r");
     if(fp == NULL){
@@ -25,24 +23,34 @@ int main()
     }
     //ストリームから1行ずつ読み込み、NULLになったら終了
     while(fgets(s, sizeof(s), fp) != NULL){
-        //1行目の時だけの処理：GETかPOST と パスの取得
-        if(count == 1){
+        //リクエストラインの解析
+        if(firstTime == 0){
             bufferMethod = strtok(s, " ");
             strcpy(method, bufferMethod);
+            fprintf(stdout, "メソッドは%s\n", method);
+            if(strcmp(method, "POST") == 0){
+                postFlag = 1;
+            }
             bufferPath = strtok(NULL, " ");
             strcpy(path, bufferPath);
+            fprintf(stdout, "パスは%s\n", path);
+            firstTime = 1;
         }
         ret = strncmp(s, "Cookie:", 7);
         if(ret == 0){
             bufferCookie = strtok(s, ":");
             bufferCookie = strtok(NULL, "\n");
             strcpy(cookie, bufferCookie);
+            fprintf(stdout, "クッキーの値は%s\n", cookie);
         }
-        count++;
-    }
-    fprintf(stdout, "メソッドは%s\n", method);
-    fprintf(stdout, "パスは%s\n", path);
-    fprintf(stdout, "クッキーの値は%s\n", cookie);
+        if(strcmp(s, "\n") == 0){
+            paragrahFlag = 1;
+            continue;
+        }
+        if(paragrahFlag == 1 && postFlag == 1){
+            fputs(s ,stdout);
+        }
+    }   
     fclose(fp);
     exit(0);
 }
